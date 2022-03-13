25d1186370518e87a9fb7d14e49462f0cea0f7fa
landofcoder <landofcoder@gmail.com>
2022-03-13 13:43:26 +0700

    updated

--- Helper/Image.php
+++ Helper/Image.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * Venustheme
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the Venustheme.com license that is
  * available through the world-wide-web at this URL:
  * http://www.venustheme.com/license-agreement.html
- * 
+ *
  * DISCLAIMER
- * 
+ *
  * Do not edit or add to this file if you wish to upgrade this extension to newer
  * version in the future.
- * 
+ *
  * @category   Venustheme
  * @package    Ves_Productlist
  * @copyright  Copyright (c) 2014 Venustheme (http://www.venustheme.com/)
@@ -45,7 +45,7 @@ class Image extends \Magento\Framework\App\Helper\AbstractHelper
 	 * @param mixed							    $file			Specific file
 	 * @return string
 	 */
-	public function getImg($product, $w=300, $h, $imgVersion='image', $file=NULL)
+	public function getImg($product, $w = 300, $h = 0, $imgVersion='image', $file=NULL)
 	{
 		if (!$h || (int)$h == 0){
 			$image = $this->_imageHelper
@@ -98,7 +98,7 @@ class Image extends \Magento\Framework\App\Helper\AbstractHelper
 			$base_image_base_name = basename($base_image->getUrl());
 			$image_array = array();
 			$tmp_base_image = false;
-			
+
 			foreach($images as $tmp_image) {
 				$tmp_image_base_name = basename($tmp_image->getFile());
 				if(!$base_image || ($tmp_image_base_name != $base_image_base_name)){
@@ -107,7 +107,7 @@ class Image extends \Magento\Framework\App\Helper\AbstractHelper
 					$tmp_base_image = $tmp_image;
 				}
 			}
-			
+
 
 			$value = ((int)$value <= 0)?1:(int)$value;
 			$image = false;
@@ -128,4 +128,4 @@ class Image extends \Magento\Framework\App\Helper\AbstractHelper
 		}
 		return '';
 	}
-}	
+}
